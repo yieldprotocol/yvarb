a839f43efdd7bf2f31b926ccb1613e03e37bf254
iamsahu <46891804+iamsahu@users.noreply.github.com>
2022-12-14 12:01:42 +0530

    fix: CAULDRON case change

--- contracts/YieldStrategyLever.sol
+++ contracts/YieldStrategyLever.sol
@@ -212,7 +212,7 @@ contract YieldStrategyLever is IERC3156FlashBorrower {
         bool success;
         if (uint32(block.timestamp) > CAULDRON.series(seriesId).maturity) {
             if (operation != Operation.REDEEM) revert OnlyRedeem();
-            address join = address(LADLE.joins(cauldron.series(seriesId).baseId));
+            address join = address(LADLE.joins(CAULDRON.series(seriesId).baseId));
 
             // Redeem:
             // Series is past maturity, borrow and move directly to collateral pool.
@@ -243,7 +243,7 @@ contract YieldStrategyLever is IERC3156FlashBorrower {
                     pool.sellFYToken(address(this), 0);
                 }
             } else if (operation == Operation.CLOSE) {
-                address join = address(LADLE.joins(cauldron.series(seriesId).baseId));
+                address join = address(LADLE.joins(CAULDRON.series(seriesId).baseId));
 
                 // Close:
                 // Series is not past maturity, borrow and move directly to collateral pool.
